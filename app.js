4a3a2e4e2635c0eb9e01178f45fa463e553eca59
Mauro Vargas <maurovargas@MacBook-Air-de-Mauro.local>
2019-12-20 12:47:02 -0500

    Add cors to app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const {User} = require('./modules');
 const {router} = require('./routes');
 const PORT = process.env.PORT || 4100;
 
+app.use(cors());
 app.use(express.urlencoded( {extended: true} ));
 app.use(express.json());
 app.use('/api/v1', router);
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
-const cors = require('cors');
 const express = require('express');
 const router = express.Router();
 const {validateToken} = require('../middleware/validations');
 const userController = require('../controllers/userController');
 
-router.post('/login', cors(), userController.userLogin);
-router.post('/create/user', cors(),  userController.createUser);
+router.post('/login', userController.userLogin);
+router.post('/create/user',  userController.createUser);
 router.use(validateToken);
-router.get('/get/all/users', cors(),  userController.getAllUsers);
-router.put('/modify/user/:userid', cors(),  userController.modifiyThisUser);
-router.delete('/delete/user/:userid', cors(),  userController.deleteThisUser);
-router.get('/', cors(),  userController.start);
-router.get('/get/user/:userid', cors(),  userController.getUser);
+router.get('/get/all/users',  userController.getAllUsers);
+router.put('/modify/user/:userid',  userController.modifiyThisUser);
+router.delete('/delete/user/:userid',  userController.deleteThisUser);
+router.get('/',  userController.start);
+router.get('/get/user/:userid',  userController.getUser);
 
 module.exports = { router };
